bd076261e3ec3e68be324eb0a2643c64df091604
Isaac Yael <sigan1998@gmail.com>
2021-09-16 17:53:53 -0500

    Avance del 80% del inicio de la aplicacion

---
+++ js/formulas.js
@@ -0,0 +1,23 @@
+const containerSelect = document.getElementById('container_select')
+const selectbox = document.getElementById('selectbox')
+const cuadro = document.getElementById('cuadro')
+const circulo = document.getElementById('circulo')
+const triangulo = document.getElementById('triangulo')
+const trianguloIsoseles = document.getElementById('trianguloIsoseles')
+var runCalculator = 0
+
+containerSelect.addEventListener('click', e=>{
+    containerSelect.classList.toggle('active')
+    selectbox.classList.toggle('selectbox_1')
+})
+
+cuadro.addEventListener('click', e=>{
+    runCalculator += 1
+    if(runCalculator === 1){
+        containerSelect.classList.toggle('active')
+        selectbox.classList.toggle('selectbox_1')
+        const main = document.getElementById('main')
+        main.classList.add('calculatorRun')
+        console.log('hola')
+    }
+})
