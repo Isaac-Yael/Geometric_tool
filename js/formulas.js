f9e41ff4f8d03297d371dcab85313fb3dc31cf45
Isaac Yael <sigan1998@gmail.com>
2021-09-26 21:06:54 -0500

    Avance, conclución de las fórmulas y el css de cada una

--- js/formulas.js
+++ js/formulas.js
@@ -17,7 +17,8 @@ cuadro.addEventListener('click', e=>{
         const main = document.getElementById('main')
         main.classList.add('calculatorRun')
     } else{
-        
+        const figuresContainer = document.querySelector('.figures_div_all')
+        figuresContainer.style.left = "0"
     }
 })
 
@@ -26,8 +27,37 @@ circulo.addEventListener('click', e=>{
     if(runCalculator === 1){
         const main = document.getElementById('main')
         main.classList.add('calculatorRun')
-        const figuresContainer = document.querySelector('.figures_container')
+        const figuresContainer = document.querySelector('.figures_div_all')
         figuresContainer.style.left = "-100%"
+    } else{
+        const figuresContainer = document.querySelector('.figures_div_all')
+        figuresContainer.style.left = "-100%"
+    }
+})
+
+triangulo.addEventListener('click', e=>{
+    runCalculator += 1;
+    if(runCalculator === 1){
+        const main = document.getElementById('main')
+        main.classList.add('calculatorRun')
+        const figuresContainer = document.querySelector('.figures_div_all')
+        figuresContainer.style.left = "-200%"
+    } else{
+        const figuresContainer = document.querySelector('.figures_div_all')
+        figuresContainer.style.left = "-200%"
+    }
+})
+
+trianguloIsoseles.addEventListener('click', e=>{
+    runCalculator += 1;
+    if(runCalculator === 1){
+        const main = document.getElementById('main')
+        main.classList.add('calculatorRun')
+        const figuresContainer = document.querySelector('.figures_div_all')
+        figuresContainer.style.left = "-300%"
+    } else{
+        const figuresContainer = document.querySelector('.figures_div_all')
+        figuresContainer.style.left = "-300%"
     }
 })
 
@@ -45,9 +75,49 @@ function calcularAyPCuadrado(){
     const areaSpan = document.getElementById('area_cuadrado')
     areaSpan.innerText = area + " cm"
 }
-// function calcularAreaCuadrado(){
-//     const input = document.getElementById('inputCuadrado').value    
-//     const area = areaCuadrada(input);
-//     const areaSpan = document.getElementById('area_cuadrado')
-//     areaSpan.innerText = area
-// }
+
+// Función para calcular el círculo
+
+diametroCirculo = (radio) => Number(radio) + Number(radio)
+const PI = Math.PI;
+
+circunferencia = (radio) => {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI
+}
+areaCirculo = (radio) => (radio * radio) * PI
+
+function calcularCyACirculo(){
+    const radio = document.getElementById('inputRadio').value
+    const areaDeCirculo = areaCirculo(radio)
+    let areaSpan = document.getElementById('area_circulo')
+    areaSpan.innerText = areaDeCirculo + " cm"
+
+    const circunferenciaCirculo = circunferencia(radio)
+    let circunferenciaSpan = document.getElementById('circunferencia_circulo')
+    circunferenciaSpan.innerText = circunferenciaCirculo + " cm"
+}
+
+// Función para calcular el triángulo
+
+perimetroTriangulo = (lado1, lado2, base) => Number(lado1) + Number(lado2) + Number(base)
+areaTriangulo = (base, altura) => (Number(base) * Number(altura)) / 2
+function calcularAyPTriangulo(){
+    const base = document.getElementById('inputBaseTriangulo').value
+    const lado1 = document.getElementById('inputLadoATriangulo').value
+    const lado2 = document.getElementById('inputLadoBTriangulo').value
+    const perimetroDeTriangulo = perimetroTriangulo(lado1, lado2, base)
+    const altura = document.getElementById('inputAlturaTriangulo').value
+    let perimetroSpan = document.getElementById('perimetro_triangulo')
+    perimetroSpan.innerText = perimetroDeTriangulo + " cm"
+
+    let areaDelTriangulo = areaTriangulo(base, altura)
+    let areaTrianguloSpan = document.getElementById('area_triangulo')
+    areaTrianguloSpan.innerText = areaDelTriangulo + " cm"
+}
+function calcularAreaTriangulo(){
+    const altura = document.getElementById('inputAlturaTriangulo').value
+    const base = document.getElementById('inputBaseTriangulo').value
+    const area = areaTriangulo(base, altura)
+    alert(area)
+}
